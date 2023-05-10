0ea3dcf302fd5d89fe141bbbbfa863a93b7881da
merlijn <merlijnvan11@gmail.com>
2023-05-10 10:26:32 +0200

    refactor
    no longer uses GetComponent in PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,30 +4,28 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
-public class PlayerController : MonoBehaviour
-{
-    private PlayerInput input;
-    private Vector2 moveInput;
-    private Rigidbody2D rb;
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private PlayerInput input;
+    [SerializeField] private Rigidbody2D rb;
+
     [SerializeField]
     [Range(0, 10)]
     private float speed = 5;
 
+    private Vector2 moveInput;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        input = GetComponent<PlayerInput>();
-        rb = GetComponent<Rigidbody2D>();
+    void Start() {
+        //input = GetComponent<PlayerInput>();
+        //rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         rb.AddForce(moveInput * speed);
     }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
+    public void OnMove(InputAction.CallbackContext context) {
         moveInput = context.ReadValue<Vector2>();
     }
 }
