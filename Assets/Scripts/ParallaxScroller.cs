a95847da5e13dfd311023d9ed99e9d09b56f05eb
marsmaantje[pc] <marsmaantje@planet.nl>
2023-05-10 18:42:15 +0200

    Parallax scrolling!!!

--- Assets/Scripts/ParallaxScroller.cs
+++ Assets/Scripts/ParallaxScroller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ParallaxScroller : MonoBehaviour
@@ -8,6 +9,9 @@ public class ParallaxScroller : MonoBehaviour
     [SerializeField] private Vector2 offset = Vector2.zero;
 
     [SerializeField][Range(-9, 10)] private int depth = 0;
+
+    public bool preview = false;
+    private bool pPreview = false;
 
     Transform cameraTransform;
 
@@ -27,11 +31,36 @@ public class ParallaxScroller : MonoBehaviour
     {
         Vector2 relativePosition = relativeObject.position;
 
-        Vector2 newPosition = new Vector2(
+        Vector3 newPosition = new Vector3(
             (relativePosition.x * scrollSpeed.x) + offset.x,
-            (relativePosition.y * scrollSpeed.y) + offset.y
+            (relativePosition.y * scrollSpeed.y) + offset.y,
+            depth
         );
 
         transform.position = newPosition;
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = new Vector3(0, 0, depth);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        //if not in play mode
+        if (Application.isPlaying) return;
+
+        //if previewing, update parallax scroll to scene view transform
+        if (preview)
+        {
+            //get scene view transform
+            Transform sceneViewTransform = SceneView.lastActiveSceneView.camera.transform;
+            UpdateParallaxScroll(sceneViewTransform);
+        }
+        else if (pPreview) //reset if we turned it off
+            ResetPosition();
+
+        pPreview = preview;
     }
 }
