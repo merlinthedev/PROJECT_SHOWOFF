fe140a2f7c1b4ccb35b63e8d85aa416f4c170ef0
merlijn <merlijnvan11@gmail.com>
2023-05-11 23:06:43 +0200

    quick playtest level improvements

--- Assets/Scripts/spike.cs
+++ Assets/Scripts/spike.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spike : MonoBehaviour
+public class Spike : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
