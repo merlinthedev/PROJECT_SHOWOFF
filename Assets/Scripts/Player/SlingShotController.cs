2775270ba500cb4632f4a8442dd0a6615b528c88
marsmaantje[laptop] <marsmaantje@planet.nl>
2023-05-15 16:41:17 +0200

    Shoot detection somewhat working

--- Assets/Scripts/Player/SlingShotController.cs
+++ Assets/Scripts/Player/SlingShotController.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SlingShotController : MonoBehaviour
 {
+    public Vector2 stickInput;
+    public Vector2 shootDirection;
+    private float lastInputTime = 0;
+
+    [SerializeField] private float maxShootForce = 10f;
+    [SerializeField] private float minShootForce = 1f;
+
+    [SerializeField] private Projectile projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +25,20 @@ public class SlingShotController : MonoBehaviour
     {
         
     }
+
+    public void OnStickInput(InputAction.CallbackContext context) {
+        Vector2 newStick = context.ReadValue<Vector2>();
+        float stickDelta = newStick.magnitude - stickInput.magnitude;
+        float deltaTime = Time.time - lastInputTime;
+        float stickSpeed = stickDelta / deltaTime;
+        if (stickSpeed < -0.5f) {
+            shootDirection = newStick;
+            Debug.Log("Shoot! " + -stickInput);
+        }
+
+
+        lastInputTime = Time.time;
+        stickInput = newStick;
+    }
+    
 }
