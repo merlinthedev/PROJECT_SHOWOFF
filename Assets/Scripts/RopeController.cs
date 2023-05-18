606b7aa9317c8d9f13866fe6ad2334caca916f71
marsmaantje[pc] <marsmaantje@planet.nl>
2023-05-18 11:27:08 +0200

    First half of the Magic WORKS!!!

--- Assets/Scripts/RopeController.cs
+++ Assets/Scripts/RopeController.cs
@@ -10,6 +10,8 @@ public class RopeController : MonoBehaviour {
 
     [SerializeField] float ropeDamping = 0f;
 
+    [SerializeField][Range(0,1)] float testProgress = 0;
+
     // Start is called before the first frame update
     void Start() {
 
@@ -22,7 +24,23 @@ public class RopeController : MonoBehaviour {
 
     public Vector2 GetRopePoint(float ropeProgress) {
         //returns the point along the rope curve at the given progress (0 to 1)
-        return Vector2.zero;
+        //we assume a constant length between rope parts
+        int ropePartCount = ropeParts.Length;
+        //make sure float is within bounds
+        ropeProgress = Mathf.Clamp01(ropeProgress) * (ropePartCount - 1);
+        //at which rope part are we
+        int ropeIndex = Mathf.FloorToInt(ropeProgress);
+        //how far are we along one rope part
+        float partProgress = ropeProgress % 1;
+        //find the rope part we are below
+        var ropePart = ropeParts[ropeIndex];
+
+        //if we are at the end of the rope
+        if(ropeIndex >= ropePartCount - 1) {
+            return ropePart.transform.position;
+        }
+        var nextRopePart = ropeParts[ropeIndex + 1];
+        return Vector2.Lerp(ropePart.transform.position, nextRopePart.transform.position, partProgress);
     }
 
     public float GetRopeProgress(Vector2 point) {
@@ -95,6 +113,10 @@ public class RopeController : MonoBehaviour {
 
     private void OnValidate() {
         UpdateRopeParts();
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(GetRopePoint(testProgress), 0.5f);
     }
 #endif
 }
