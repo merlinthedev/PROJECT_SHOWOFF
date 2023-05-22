073edc2d19c49906fbbaf804b04359cb14f6542a
Fronter <mathijs.klingeman@gmail.com>
2023-05-22 15:53:15 +0200

    Branch visuals

--- Assets/BranchScript.cs
+++ Assets/BranchScript.cs
@@ -6,27 +6,44 @@ public class BranchScript : MonoBehaviour
 {
     [SerializeField] private bool isParent = false;
     public bool gotBonked = false;
+    public bool fade = false;
 
     private void Update() {
 
+        // If this is a parent branch, check if any of the children have been bonked
         if(isParent)
-        { 
+        {
+            //try to get a rigidbody2d component
+            if(TryGetComponent<Rigidbody2D>(out Rigidbody2D rb)) rb.bodyType = RigidbodyType2D.Kinematic;
             for (int i = 0; i < transform.childCount; i++) 
             {
                 if (transform.GetChild(i).GetComponent<BranchScript>().gotBonked == true)
                     Destroy();
             }
         }
+        //if fade is true, fade out the branch and then destroy it
+        if(fade) 
+        {
+            Color tmp = GetComponent<SpriteRenderer>().color;
+            if(tmp.a > 0) tmp.a -= 0.002f;
+            else if (tmp.a < 0) tmp.a = 0;
+            GetComponent<SpriteRenderer>().color = tmp;
+            //destroy the branch once it is fully faded out
+            if(tmp.a == 0) Destroy(gameObject);
+            Debug.Log(tmp.a);
+        }
     }
+    //destroy all the fixed joints and fade out each individual the branch
     public void Destroy() {
-        for (int i = 0; i < transform.childCount; i++) {
+        for (int i = 0; i < transform.childCount; i++) 
+        {
             transform.GetChild(i).GetComponent<FixedJoint2D>().enabled = false;
+            transform.GetChild(i).GetComponent<BranchScript>().fade = true;
         }
 
     }
+    //if the branch collides with a stone, set gotBonked to true
     private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.name == ("stone")) {
-            gotBonked=true;
-        }
+        if (collision.gameObject.name == ("stone")) gotBonked = true;
     }
 }
