939ac26e5c0fd051209040d23704806ecb7dd349
merlijn <merlijnvan11@gmail.com>
2023-05-29 10:51:13 +0200

    water

---
+++ Assets/Scripts/Console/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Console {
+    public class Message {
+        private Object origin;
+        private string message;
+        private MessageType type;
+
+        public Message(string message, Object origin, MessageType messageType) {
+            this.message = message;
+            this.origin = origin;
+            this.type = messageType;
+        }
+
+        public MessageType getMessageType() {
+            return this.type;
+        }
+
+        public enum MessageType {
+            Debug,
+            Log,
+            Warning,
+            Error,
+            Exception,
+        }
+    }
+
+}
