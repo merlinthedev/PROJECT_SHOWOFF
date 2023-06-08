f806e89b9ebb7177688ef4f64cbfcc6918a42080
merlijn <merlijnvan11@gmail.com>
2023-06-08 11:28:35 +0200

    slope

--- Assets/Scripts/BetterPlayerMovement.cs
+++ Assets/Scripts/BetterPlayerMovement.cs
@@ -9,6 +9,7 @@ public class BetterPlayerMovement : MonoBehaviour {
     private Rigidbody2D m_Rigidbody2D;
 
     [SerializeField] private CapsuleCollider2D m_CapsuleCollider2D;
+    [SerializeField] private PhysicsMaterial2D m_PlayerPhysicsMaterial2D;
     [SerializeField] private float maximumHorizontalSpeed = 10f;
     [SerializeField] private float groundAcceleration;
     [SerializeField] private float airAcceleration;
@@ -45,30 +46,68 @@ public class BetterPlayerMovement : MonoBehaviour {
         horizontalMovement();
 
         isGrounded = false;
+        slopeAngle = 180;
     }
 
     private void horizontalMovement() {
+        if (isGrounded && slopeAngle > maximumGroundAngle) {
+            isGrounded = false;
+            m_PlayerPhysicsMaterial2D.friction = 0f;
+        } else {
+            m_PlayerPhysicsMaterial2D.friction = 1f;
+        }
+
         float rawXMovement = movementInput.x;
 
-        float desiredHorizontalSpeed = rawXMovement * maximumHorizontalSpeed;
-        float velocityGap = desiredHorizontalSpeed - m_Rigidbody2D.velocity.x;
+        if (rawXMovement != 0 || isGrounded) {
+            float desiredHorizontalSpeed = rawXMovement * maximumHorizontalSpeed;
+            float velocityGap = desiredHorizontalSpeed - m_Rigidbody2D.velocity.x;
 
-        float acceleration = isGrounded ? groundAcceleration : airAcceleration;
-        float accelerationThisFrame = acceleration * Time.fixedDeltaTime;
-        float accelerationSign = Mathf.Sign(velocityGap);
-        float accelerationMagnitude = Mathf.Min(Mathf.Abs(velocityGap), accelerationThisFrame);
+            float acceleration = isGrounded ? groundAcceleration : airAcceleration;
+            float accelerationThisFrame = acceleration * Time.fixedDeltaTime;
+            float accelerationSign = Mathf.Sign(velocityGap);
+            float accelerationMagnitude = Mathf.Min(Mathf.Abs(velocityGap), accelerationThisFrame);
 
-        Vector2 accelerationVector = new Vector2(accelerationMagnitude * accelerationSign, 0f);
-        m_Rigidbody2D.AddForce(accelerationVector, ForceMode2D.Impulse);
+            Vector2 accelerationVector = new Vector2(accelerationMagnitude * accelerationSign, 0f);
+
+            // rotate acceleration vector to match slope using the slope normal
+            if (isGrounded && slopeAngle < maximumGroundAngle) {
+                accelerationVector = Quaternion.FromToRotation(Vector2.up, slopeNormal) * accelerationVector;
+            }
+
+            m_Rigidbody2D.AddForce(accelerationVector, ForceMode2D.Impulse);
+        }
     }
 
     private bool isGrounded;
+    public float slopeAngle;
+    private Vector2 slopeNormal;
+    private float lastGroundedTime;
 
     private void OnCollisionStay2D(Collision2D other) {
         if (Utils.IsInLayerMask(other.gameObject.layer, groundLayer)) {
             isGrounded = true;
-        }
 
+            //find most vertical facing contact point
+            ContactPoint2D mostVerticalContactPoint = other.contacts[0];
+            for (int i = 1; i < other.contactCount; i++) {
+                ContactPoint2D contactPoint = other.contacts[i];
+                if (Vector2.Dot(contactPoint.normal, Vector2.up) >
+                    Vector2.Dot(mostVerticalContactPoint.normal, Vector2.up)) {
+                    mostVerticalContactPoint = contactPoint;
+                }
+            }
+
+            Vector2 groundNormal = mostVerticalContactPoint.normal;
+
+            float newSlopeAngle = Vector2.Angle(Vector2.up, groundNormal);
+            if (newSlopeAngle < slopeAngle) {
+                slopeAngle = newSlopeAngle;
+                slopeNormal = groundNormal;
+            }
+
+            lastGroundedTime = Time.time;
+        }
     }
 
     private Vector2 movementInput;
