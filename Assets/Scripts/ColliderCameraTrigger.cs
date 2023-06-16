ea1590355d9249de8fd974c546885609c59b5a9f
marsmaantje[laptop] <marsmaantje@planet.nl>
2023-06-16 12:27:43 +0200

    CameraBlendStuff, not all works yet...

--- Assets/Scripts/ColliderCameraTrigger.cs
+++ Assets/Scripts/ColliderCameraTrigger.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 
 public class ColliderCameraTrigger : MonoBehaviour
 {
-    [SerializeField] new private Cinemachine.CinemachineVirtualCameraBase camera;
-    [SerializeField] private int cameraPriority = 5;
+    [SerializeField] private Cinemachine.CinemachineVirtualCamera vcam;
+    [SerializeField] private Cinemachine.CinemachineMixingCamera mixCam;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (camera == null)
-            camera = GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
-        
-        camera.Priority = -1;
+    [SerializeField] bool playerInRange = false;
+    [SerializeField] float blendStart = 5f;
+    [SerializeField] float blendEnd = 1f;
+    
+    private GameObject player;
+
+    [SerializeField] AnimationCurve blendCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
+
+
+    private void Update() {
+        if(playerInRange) {
+            float distance = Vector2.Distance(transform.position, player.transform.position);
+            float blendWeight = blendCurve.Evaluate(Mathf.InverseLerp(blendStart, blendEnd, distance));
+            mixCam.SetWeight(vcam, blendWeight);
+        } else {
+            mixCam.SetWeight(vcam, 0);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        //Debug.Log("Object entered with tag: " + other.gameObject.tag);
         if (other.gameObject.tag == "Player")
         {
-            camera.Priority = cameraPriority;
+            playerInRange = true;
+            player = other.gameObject;
         }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
+        Debug.Log("Trigger entered with tag: " + other.gameObject.tag);
         if (other.gameObject.tag == "Player")
         {
-            camera.Priority = -1;
+            playerInRange = false;
+            player = null;
         }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, blendStart);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, blendEnd);
     }
 }
